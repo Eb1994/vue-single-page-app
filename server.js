5680811085c81c68f8d130cd370479bfe247074f
EMMETT\Emmett Brogan <emmett-b@hotmail.com>
2018-03-14 12:00:18 +0000

    installed web server with express.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHTML = (function() {
+  return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
+})();
+
+app.get("*", function (req, res) {
+  res.write(indexHTML);
+  res.end();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server started at http://localhost:${port}`);
+});
